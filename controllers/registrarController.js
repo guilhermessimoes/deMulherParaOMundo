07766aa3e13175f19001c91ffb08743e6d9a76dd
Guilherme <guilhermejunios@gmail.com>
2021-10-27 14:25:42 -0300

    Alteracores no front end

--- controllers/registrarController.js
+++ controllers/registrarController.js
@@ -1,6 +1,28 @@
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+
 const registrarController = {
     viewForm: (req, res) => {      
         res.render('registrar')
+    },
+
+    loginPost: (req, res) => {
+        let listaDeErros = validationResult(req)
+        if(!listaDeErros.isEmpty()){
+            const alert = listaDeErros.array()
+            res.render("registrar", {alert: alert})
+        }
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const usuarioLogado = usuarios.find((usuario)=>usuario.email == email && bcrypt.compareSync(password, usuario.password)) 
+        if (usuarioLogado != null) {
+            req.session.usuario_logado= true
+            res.redirect("/")
+            console.log(usuarioLogado)
+        }else{
+            res.render("registrar")
+        }
     },
 }
 
---
+++ middlewares/registrarMiddleware.js
@@ -0,0 +1,18 @@
+var { check } = require("express-validator");
+
+const loginValidator = [
+    check("email").isEmail().notEmpty().withMessage("O campo email é obrigatório."),
+    check("senha").notEmpty().withMessage("O campo senha é obrigatório.")
+];
+
+const esqueceuLoginValidator = [
+    check("email").isEmail().notEmpty().withMessage("O campo email é obrigatório.")
+];
+
+const createLoginValidator = [
+    check("nome").isLength({min:3, max:40}).withMessage("O campo nome tem que conter no mínimo 3 carcterer."),
+    check("email").isEmail().notEmpty().withMessage("O campo email é obrigatório."),
+    check("senha").notEmpty().withMessage("O campo senha é obrigatório."),
+]
+
+module.exports = {loginValidator, createLoginValidator, esqueceuLoginValidator}
