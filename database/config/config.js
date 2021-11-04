11d6999a8a9a7ce8963e30c85500e22ae1490ca6
Guilherme <guilhermejunios@gmail.com>
2021-11-04 18:06:36 -0300

    Ajustando o front com erro de exibicao

--- database/config/config.js
+++ database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "uquygxrnujydzd",
+    "password": "696d1291170f24ac2cba6e34024104dbe348ea97e300a493a26fa27c10a019a7",
+    "database": "d69t81vrlpia42",
+    "host": "ec2-35-169-204-98.compute-1.amazonaws.com",
     "dialect": "mysql"
   }
 }
